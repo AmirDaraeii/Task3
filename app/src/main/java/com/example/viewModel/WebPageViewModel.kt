b6d05ac72259251198b796ba764a82f85a435f76
ADaraei <amiir.daraeii@gmail.com>
2024-07-13 13:15:13 +0330

    init

---
+++ app/src/main/java/com/example/viewModel/WebPageViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.viewModel
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+
+class WebPageViewModel {
+
+//    val url =
+//        "https://blog.mindorks.com/what-are-the-different-protection-levels-in-android-permission/"
+//    val client = OkHttpClient()
+//    val result = StringBuilder()
+
+
+
+
+
+
+}
